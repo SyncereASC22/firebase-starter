47a5f87aa8cd5129006d1c4f35c13ed173205c4f
syncere.mcneil1@gmail.com <Syncere>
2022-08-09 15:32:25 -0400

    created chatbox

--- init-firebase.js
+++ init-firebase.js
@@ -3,8 +3,14 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
-};
+    apiKey: "AIzaSyCfehoMxdDkakquO-ZOxlTd7lhPQK4aKAA",
+    authDomain: "fire-messages-4bc10.firebaseapp.com",
+    databaseURL: "https://fire-messages-4bc10-default-rtdb.firebaseio.com",
+    projectId: "fire-messages-4bc10",
+    storageBucket: "fire-messages-4bc10.appspot.com",
+    messagingSenderId: "905691037475",
+    appId: "1:905691037475:web:57cf4d98beb4d10bc09696"
+  };
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
